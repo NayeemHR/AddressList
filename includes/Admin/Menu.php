f5b70f896b58d3406dfca5e90f309e45eaa1a7cf
Nayeem HR <nayeemhasan042@yahoo.com>
2020-04-02 02:06:21 +0600

    6th tutorial complete

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -26,15 +26,23 @@ class Menu{
     public function admin_menu(){
         $parent_slug='wedevs-academy';
         $capability='manage_options';
-        add_menu_page( __( 'weDevs Academy', 'wedevs-academy' ), __('Academy', 'wedevs-academy'), $capability, $parent_slug, [$this->addressbook, 'plugin_page'], 'dashicons-welcome-learn-more' );
+        $hook = add_menu_page( __( 'weDevs Academy', 'wedevs-academy' ), __('Academy', 'wedevs-academy'), $capability, $parent_slug, [$this->addressbook, 'plugin_page'], 'dashicons-welcome-learn-more' );
 
         add_submenu_page( $parent_slug, __('Adressbook', 'wedevs-academy'), __('Address Book', 'wedevs-academy'), $capability, $parent_slug, [ $this->addressbook, 'plugin_page']);
 
         add_submenu_page( $parent_slug, __('Settings', 'wedevs-academy'), __('Settings', 'wedevs-academy'), $capability, 'wedevs-academy-settings', [ $this, 'settings_page']);
+
+        add_action( 'admin_head-' . $hook, [$this, 'enqueue_assets'] );
+
+
     }
         
 
     public function settings_page(){
         echo 'this is addressbook';
+    }
+
+    public function enqueue_assets(){
+        wp_enqueue_style( 'academy-admin-style' );
     }
 }
--- includes/Assets.php
+++ includes/Assets.php
@@ -19,15 +19,20 @@ class Assets{
             'academy-script'=>[
                 'src' => WD_ACADEMY_ASSETS . '/js/frontend.js',
                 'version' => filemtime( WD_ACADEMY_PATH . '/assets/js/frontend.js'), 
+                'deps' => ['jquery']
             ]
         ];
     }
 
     public function get_styles(){
         return[
-            'academy-script'=>[
+            'academy-style'=>[
                 'src' => WD_ACADEMY_ASSETS . '/css/frontend.css',
                 'version' => filemtime( WD_ACADEMY_PATH . '/assets/css/frontend.css'), 
+            ],
+            'academy-admin-style'=>[
+                'src' => WD_ACADEMY_ASSETS . '/css/admin.css',
+                'version' => filemtime( WD_ACADEMY_PATH . '/assets/css/admin.css'), 
             ]
         ];
     }
@@ -36,14 +41,19 @@ class Assets{
         $scripts = $this->get_scripts();
 
         foreach ($scripts as $handle => $script) {
-            wp_register_script( 'academy-script', WD_ACADEMY_ASSETS . '/js/frontend.js', false, filemtime( WD_ACADEMY_PATH . '/assets/js/frontend.js'), true);
+            $deps = isset($script['deps']) ? $script['deps'] : false;
+            
+            wp_register_script( $handle, $script['src'], $deps, $script['version'], true);
 
         }
         $styles = $this->get_styles();
-        wp_register_style( 'academy-style', WD_ACADEMY_ASSETS . '/css/frontend.css', false, filemtime( WD_ACADEMY_PATH . '/assets/css/frontend.css'));
 
+        foreach ($styles as $handle => $style) {
+            $deps = isset($style['deps']) ? $style['deps'] : false;
+            
+            wp_register_style( $handle, $style['src'], $deps, $style['version'] );
 
-        // wp_enqueue_script( 'academy-script' );
-        // wp_enqueue_style( 'academy-style' );
+        }
+
     }
 }
