70e3e56ef6869aed89846990fe6b84f9b729d191
Nayeem HR <nayeemhasan042@yahoo.com>
2020-04-05 19:39:44 +0600

    9th complete

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -5,33 +5,60 @@ namespace WeDevs\Academy;
 /**
  * ajax handler class
  */
-
 class Ajax{
-    function __construct()
-    {
+
+    /**
+     * Class Constructor
+     */
+    function __construct() {
         add_action( 'wp_ajax_wd_academy_enquiry', [ $this, 'submit_enquiry'] );
         add_action( 'wp_ajax_nopriv_wd_academy_enquiry', [ $this, 'submit_enquiry'] );
+        
         add_action( 'wp_ajax_wd-academy-delete-contact', [ $this, 'delete_contact'] );
     }
 
-    function submit_enquiry(){
-
-        if(!wp_verify_nonce( $_REQUEST['_wpnonce'], 'wd-ac-enquiry-form' )){
+    /**
+     * Handle Enquiry Submission
+     * 
+     * @return void
+     */
+    public function submit_enquiry() {
+        if( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'wd-ac-enquiry-form' )){
             wp_send_json_error([
-                'message' => 'Nonce verification failed!'
-            ]);
+                'message' => __( 'Nonce verification failed!', 'wedevs-academy' )
+            ] );
         }
-        wp_send_json_success( [
-            'message' => 'Enquiry has been sent successfully.'
-        ] );
 
+        wp_send_json_success([
+            'message' => __( 'Enquiry has been sent successfully!', 'wedevs-academy' )
+        ]);
         // wp_send_json_error([
         //     'message' => 'Somthing is wrong.'
         // ]);
-
-        
     }
-    public function delete_contact(){
+
+    /**
+     * Handle Contact Deletion
+     * 
+     * @return void
+     */
+    public function delete_contact() {
+        if( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'wd-ac-admin-nonce' )){
+            wp_send_json_error([
+                'message' => __( 'Nonce verification failed!', 'wedevs-academy' )
+            ] );
+        }
+        
+        if ( ! current_user_can( 'manage_options' ) ) {
+            wp_send_json_error( [
+                'message' => __( 'No permission!', 'wedevs-academy' )
+            ] );
+        }
+
+        $id = isset( $_REQUEST['id'] ) ? intval( $_REQUEST['id'] ) : 0;
+        wd_ac_delete_address( $id );
+
+
         wp_send_json_success();
     }
 }
--- includes/functions.php
+++ includes/functions.php
@@ -2,33 +2,33 @@
 
 /**
  * Insert a new address
- * 
- * @param array $args 
- * 
+ *
+ * @param  array  $args
+ *
  * @return int|WP_Error
- * 
  */
-
-function wd_ac_insert_address( $args= [] ){
+function wd_ac_insert_address( $args = [] ) {
     global $wpdb;
 
-    if ( empty( $args['name'] ) ){
-        return new \WP_Error('no-name', __('You must provide a name.', 'wedevs-academy') );
+    if ( empty( $args['name'] ) ) {
+        return new \WP_Error( 'no-name', __( 'You must provide a name.', 'wedevs-academy' ) );
     }
 
     $defaults = [
-        'name'     => '',
-        'address'  => '',
-        'phone'    => '',
-        'created_by'=> get_current_user_id(),
-        'created_at'=> current_time('mysql'),
+        'name'       => '',
+        'address'    => '',
+        'phone'      => '',
+        'created_by' => get_current_user_id(),
+        'created_at' => current_time( 'mysql' ),
     ];
 
     $data = wp_parse_args( $args, $defaults );
 
-    if (isset($data['id'])) {
-        $id = $data['id']; 
-        unset($data['id']);
+    if ( isset( $data['id'] ) ) {
+
+        $id = $data['id'];
+        unset( $data['id'] );
+
         $updated = $wpdb->update(
             $wpdb->prefix . 'ac_addresses',
             $data,
@@ -38,12 +38,15 @@ function wd_ac_insert_address( $args= [] ){
                 '%s',
                 '%s',
                 '%d',
-                '%s',
+                '%s'
             ],
             [ '%d' ]
         );
 
+        wd_ac_address_purge_cache( $id );
+
         return $updated;
+
     } else {
 
         $inserted = $wpdb->insert(
@@ -54,90 +57,136 @@ function wd_ac_insert_address( $args= [] ){
                 '%s',
                 '%s',
                 '%d',
-                '%s',
+                '%s'
             ]
         );
-    
-        if( ! $inserted ){
-            return new \WP_Error('failed-to-insert', __( 'Failed to insert data', 'wedevs-academy' ) );
+
+        if ( ! $inserted ) {
+            return new \WP_Error( 'failed-to-insert', __( 'Failed to insert data', 'wedevs-academy' ) );
         }
-    
+
+        wd_ac_address_purge_cache();
+
         return $wpdb->insert_id;
-    
-
-
     }
-
-
 }
-
 
 /**
  * Fetch Addresses
- * 
- * @param array $args
- * 
+ *
+ * @param  array  $args
+ *
  * @return array
  */
-function wd_ac_get_addresses( $args = []){
+function wd_ac_get_addresses( $args = [] ) {
     global $wpdb;
 
     $defaults = [
-        'number' => 20,
-        'offset' => 0,
+        'number'  => 20,
+        'offset'  => 0,
         'orderby' => 'id',
-        'order' => 'ASC'
+        'order'   => 'ASC'
     ];
 
     $args = wp_parse_args( $args, $defaults );
 
-    $sql= $wpdb->prepare(
-        "SELECT * FROM {$wpdb->prefix}ac_addresses
-        ORDER BY {$args['orderby']} {$args['order']}
-        LIMIT %d, %d",
-        $args['offset'],$args['number']
+    $last_changed = wp_cache_get_last_changed( 'address' );
+    $key          = md5( serialize( array_diff_assoc( $args, $defaults ) ) );
+    $cache_key    = "all:$key:$last_changed";
+
+    $sql = $wpdb->prepare(
+            "SELECT * FROM {$wpdb->prefix}ac_addresses
+            ORDER BY {$args['orderby']} {$args['order']}
+            LIMIT %d, %d",
+            $args['offset'], $args['number']
     );
 
-    $items = $wpdb->get_results( $sql );
+    $items = wp_cache_get( $cache_key, 'address' );
 
-    return $items; 
+    if ( false === $items ) {
+        $items = $wpdb->get_results( $sql );
 
+        wp_cache_set( $cache_key, $items, 'address' );
+    }
+
+    return $items;
 }
 
 /**
- * get the count of total address
- * 
+ * Get the count of total address
+ *
  * @return int
  */
-
-function wd_ac_address_count(){
+function wd_ac_address_count() {
     global $wpdb;
 
-    return (int) $wpdb->get_var( "SELECT count(id) FROM {$wpdb->prefix}ac_addresses");
+    $count = wp_cache_get( 'count', 'address' );
+
+    if ( false === $count ) {
+        $count = (int) $wpdb->get_var( "SELECT count(id) FROM {$wpdb->prefix}ac_addresses" );
+
+        wp_cache_set( 'count', $count, 'address' );
+    }
+
+    return $count;
 }
 
 /**
- * Fetch a single contact from  the DB
- * 
- * @param int $id
- * 
- * @return object 
+ * Fetch a single contact from the DB
+ *
+ * @param  int $id
+ *
+ * @return object
  */
-
 function wd_ac_get_address( $id ) {
     global $wpdb;
 
-    return $wpdb->get_row(
-        $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}ac_addresses WHERE id= %d", $id )
-    );
+    $address = wp_cache_get( 'book-' . $id, 'address' );
+
+    if ( false === $address ) {
+        $address = $wpdb->get_row(
+            $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}ac_addresses WHERE id = %d", $id )
+        );
+
+        wp_cache_set( 'book-' . $id, $address, 'address' );
+    }
+
+    return $address;
 }
 
-function wd_ac_delete_address( $id ){
+/**
+ * Delete an address
+ *
+ * @param  int $id
+ *
+ * @return int|boolean
+ */
+function wd_ac_delete_address( $id ) {
     global $wpdb;
+
+    wd_ac_address_purge_cache( $id );
 
     return $wpdb->delete(
         $wpdb->prefix . 'ac_addresses',
-        [ 'id' => $id],
+        [ 'id' => $id ],
         [ '%d' ]
     );
+}
+
+/**
+ * Purge the cache for books
+ *
+ * @param  int $book_id
+ *
+ * @return void
+ */
+function wd_ac_address_purge_cache( $book_id = null ) {
+    $group = 'address';
+
+    if ( $book_id ) {
+        wp_cache_delete( 'book-' . $book_id, $group );
+    }
+
+    wp_cache_delete( 'count', $group );
+    wp_cache_set( 'last_changed', microtime(), $group );
 }
